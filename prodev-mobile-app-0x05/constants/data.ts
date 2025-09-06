04e9188e836b965e168a06565af393a170554a8d
Eddy Ckedar <eddykedar@gmail.com>
2025-09-06 09:45:16 +0200

    Create data.ts

---
+++ prodev-mobile-app-0x05/constants/data.ts
@@ -0,0 +1,19 @@
+// constants/data.ts
+import { Property } from "@/interfaces";
+
+export const properties: Property[] = [
+  {
+    id: 1,
+    title: "Luxury Villa",
+    location: "Kigali, Rwanda",
+    price: 1200,
+    image: require("@/assets/images/mansion.png"),
+  },
+  {
+    id: 2,
+    title: "Modern Apartment",
+    location: "Nairobi, Kenya",
+    price: 800,
+    image: require("@/assets/images/sample-image.png"),
+  },
+];
