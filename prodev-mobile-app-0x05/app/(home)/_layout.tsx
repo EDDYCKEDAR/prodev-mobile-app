7332e81b54863e0862c0705f946e12a4bdcdafcb
Eddy Ckedar <eddykedar@gmail.com>
2025-09-06 09:46:38 +0200

    Update _layout.tsx

--- prodev-mobile-app-0x05/app/(home)/_layout.tsx
+++ prodev-mobile-app-0x05/app/(home)/_layout.tsx
@@ -1,18 +1,24 @@
-import { AntDesign, EvilIcons, Feather, FontAwesome, Ionicons } from "@expo/vector-icons";
+// app/(home)/layout.tsx
 import { Tabs } from "expo-router";
+import { Ionicons } from "@expo/vector-icons";
 
-
-const HomeRootLayout = () => {
-return (
-<Tabs screenOptions={{ tabBarActiveTintColor: '#34967C', headerShown: false }}>
-<Tabs.Screen name="index" options={{ title: 'Home', tabBarIcon: ({ color }) => <AntDesign name="home" size={24} color={color} /> }} />
-<Tabs.Screen name="search" options={{ title: 'Search', headerShown: true, tabBarIcon: ({ color }) => <Feather name="search" size={24} color={color} /> }} />
-<Tabs.Screen name="saved" options={{ title: 'Saved', headerShown: true, tabBarIcon: ({ color }) => <EvilIcons name="heart" size={27} color={color} /> }} />
-<Tabs.Screen name="inbox" options={{ title: 'Inbox', headerShown: true, tabBarIcon: ({ color }) => <Ionicons name="chatbubbles-outline" size={24} color={color} /> }} />
-<Tabs.Screen name="profile" options={{ title: 'Profile', headerShown: true, tabBarIcon: ({ color }) => <FontAwesome name="user-o" size={24} color={color} /> }} />
-</Tabs>
-);
+export default function HomeLayout() {
+  return (
+    <Tabs screenOptions={{ headerShown: false }}>
+      <Tabs.Screen
+        name="index"
+        options={{
+          tabBarLabel: "Home",
+          tabBarIcon: ({ color, size }) => <Ionicons name="home" size={size} color={color} />,
+        }}
+      />
+      <Tabs.Screen
+        name="profile"
+        options={{
+          tabBarLabel: "Profile",
+          tabBarIcon: ({ color, size }) => <Ionicons name="person" size={size} color={color} />,
+        }}
+      />
+    </Tabs>
+  );
 }
-
-
-export default HomeRootLayout;
